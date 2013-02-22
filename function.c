0ccf99d117ada2c12722aff121252fd1948101f6
Linden Ralph <lindenr@fastmail.fm>
2013-02-22 19:26:32 +0000

    README IS WRONG; fixed bug, README changes coming later.

--- function.c
+++ function.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "vector.h"
 
@@ -142,17 +143,103 @@ Vector argify (Vector tokens)
 	return types;
 }
 
+#define PT(str) pt(incfile, FNUM, tokens, types, str)
+#define LP(str) lp(incfile, FNUM, tokens, types, str)
+
+void pt (FILE *file, int FNUM, Vector tokens, Vector types, const char *str)
+{
+	char out[1000];
+	char FUNC[50];
+	sprintf (FUNC, "FUNCTION%d", FNUM);
+	int i, cur, len = strlen(str);
+	for (i = 0, cur = 0; i < len; ++ i)
+	{
+		if (str[i] != '$')
+		{
+			out[cur] = str[i];
+			++ cur;
+			continue;
+		}
+		switch (str[i+1])
+		{
+			case 'F':
+			{
+				strcpy ((char*)(out+cur), FUNC);
+				cur += strlen(FUNC);
+				break;
+			}
+			case 'A':
+			{
+				union Token *t = v_at (tokens, 0);
+				strcpy ((char*)(out+cur), t->str.string);
+				cur += strlen (t->str.string);
+				break;
+			}
+		}
+		++ i;
+	}
+	out[cur] = 0;
+	//fprintf (stderr, "%s", out);
+	fprintf (file, "%s", out);
+}
+
+void lp (FILE *file, int FNUM, Vector tokens, Vector types, const char *str)
+{
+	int i;
+	char out[1000];
+	for (i = 0; i < types->len; ++ i)
+	{
+		int j, len = strlen(str), k;
+		char NUM[50]; sprintf(NUM, "%d", i);
+		for (j = 0, k = 0; j < len; ++ j)
+		{
+			if (str[j] != '$')
+			{
+				out[k] = str[j];
+				++ k;
+				continue;
+			}
+			switch (str[j+1])
+			{
+				case 'N':
+				{
+					strcpy ((char*)(out+k), NUM);
+					k += strlen (NUM);
+					break;
+				}
+				case 'T':
+				{
+					strcpy ((char*)(out+k), v_at (types, i));
+					k += strlen (v_at (types, i));
+					break;
+				}
+				case 'C':
+				{
+					if (i == (types->len - 1))
+						break;
+					out[k] = ',';
+					++ k;
+					break;
+				}
+			}
+			++ j;
+		}
+		out[k] = 0;
+		pt (file, FNUM, tokens, types, out);
+	}
+}
+
 int main (int argc, char **argv)
 {
 	if (argc != 3)
 		exit (1);
 	FILE *ifile = fopen (argv[2], "rb");
-	FILE *tfile = tmpfile ();
-	FILE *cfile = tmpfile ();
+	FILE *incfile = tmpfile ();
+	FILE *tofile = tmpfile ();
 	FILE *ofile = fopen (argv[1], "wb");
 	char c = ' ';
 	int FNUM = 0, i;
-	fprintf (cfile, "#include <string.h>\n#include <stdint.h>\n#include <stdlib.h>\ntypedef void (*TYPFUNC) ();\n");
+	fprintf(incfile, "#include <malloc.h>\n");
 	while (!feof(ifile))
 	{
 		while (!feof(ifile))
@@ -160,14 +247,20 @@ int main (int argc, char **argv)
 			c = fgetc (ifile);
 			while ((!feof(ifile)) && c != '$')
 			{
-				fputc (c, tfile);
+				fputc (c, tofile);
 				c = fgetc (ifile);
 			}
 			if (feof (ifile))
+				goto DONE;
+			c = fgetc (ifile);
+			if (c == '.')
 				break;
-			if (fgetc (ifile) == '$')
-				break;
-			fputc ('$', tfile);
+			else if (c == '$')
+			{
+				fprintf (tofile, "FUNCTION%dcallback", ++FNUM);
+				continue;
+			}
+			fputc ('$', tofile);
 			fseek (ifile, -1, SEEK_CUR);
 		}
 		if (feof (ifile))
@@ -179,75 +272,62 @@ int main (int argc, char **argv)
 			break;
 		if (c != '(')
 		{
-			fprintf (stderr, "function: extected '('\n");
+			fprintf (stderr, "function: expected '('\n");
 			exit (1);
 		}
-		fprintf (tfile, "FUNCTION%d (", ++FNUM);
+		fprintf (tofile, "FUNCTION%d (", FNUM);
 		Vector tokens = tokenise(ifile);
 		for (i = 2; i < tokens->len; ++ i)
 		{
-			output (tfile, v_at(tokens, i));
+			output (tofile, v_at(tokens, i));
 		}
 		Vector types = argify (tokens);
-/*		printf ("\nSTART\n");
+		/*printf ("\nSTART\n");
 		for (i = 0; i < types->len; ++ i)
 		{
 			printf ("%s\n", v_at(types, i));
 		}
 		printf ("END\n");*/
-		fprintf (cfile, "void *FUNCTION%ddata;\nvoid FUNCTION%dcallback ()\n{\n\t", FNUM, FNUM);
-		for (i = 0; i < types->len; ++ i)
-			fprintf (cfile, "%s var%d; ", v_at (types, i), i);
-		fprintf (cfile, "\n\tuintptr_t i = (uintptr_t)FUNCTION%ddata;\n", FNUM);
-		for (i = 0; i < types->len; ++ i)
-			fprintf (cfile, "\tmemcpy (&var%d, (void*)i, sizeof(var%d)); i += sizeof(var%d);\n", i, i, i);
-		fprintf (cfile, "\tfree (FUNCTION%ddata);\n\t", FNUM);
-		output (cfile, v_at (tokens, 0));
-		fprintf (cfile, "(");
-		for (i = 0; i < types->len; ++ i)
-			fprintf (cfile, "%svar%d", (i>0)?", ":"", i);
-		fprintf (cfile, ");\n}\n");
-		fprintf (cfile, "TYPFUNC FUNCTION%d (", FNUM);
-		for (i = 0; i < types->len; ++ i)
-			fprintf (cfile, "%s%s var%d", (i>0)?", ":"", v_at (types, i), i);
-		fprintf (cfile, ")\n{\n");
-		fprintf (cfile, "\tFUNCTION%ddata = malloc (", FNUM);
-		for (i = 0; i < types->len; ++ i)
-			fprintf (cfile, "%ssizeof (var%d)", (i>0)?" + ":"", i);
-		fprintf (cfile, ");\n");
-		fprintf (cfile, "\tuintptr_t i = (uintptr_t)FUNCTION%ddata;\n", FNUM);
-		for (i = 0; i < types->len; ++ i)
-			fprintf (cfile, "\tmemcpy ((void*)i, &var%d, sizeof(var%d)); i += sizeof(var%d);\n", i, i, i);
-		fprintf (cfile, "\treturn &FUNCTION%dcallback;\n}\n\n", FNUM);
+		PT("struct $Fdata\n{\n\t");
+		LP("$T var$N; ");
+		PT("\n};\nvoid $Fcallback (void *data)\n{\n\tstruct $Fdata *F = data;\n\t$A (");
+		LP("F->var$N$C");
+		PT(");\n\tfree(F);\n}\nvoid *$F(");
+		LP("$T var$N$C");
+		PT(")\n{\n\tstruct $Fdata *F = malloc(sizeof(*F));\n");
+		LP("\tF->var$N = var$N;\n");
+		PT("\treturn F;\n}\n\n");
 	}
-	fseek (tfile, 0, SEEK_SET);
-	while (!feof(tfile))
+DONE:
+	fseek (tofile, 0, SEEK_SET);
+	while (!feof(tofile))
 	{
-		c = fgetc (tfile);
-		while ((!feof(tfile)) && c != '$')
+		c = fgetc (tofile);
+		while ((!feof(tofile)) && c != '$')
 		{
 			fputc (c, ofile);
-			c = fgetc (tfile);
+			c = fgetc (tofile);
 		}
-		if (feof (tfile))
+		if (feof (tofile))
 			exit (0);
-		if (fgetc (tfile) == '#')
+		c = fgetc (tofile);
+		if (c == '#')
 			break;
-		fputc ('$', tfile);
+		fputc ('$', tofile);
 		fseek (ifile, -1, SEEK_CUR);
 	}
-	fseek (cfile, 0, SEEK_SET);
-	c = fgetc (cfile);
-	while (!feof(cfile))
+	fseek (incfile, 0, SEEK_SET);
+	c = fgetc (incfile);
+	while (!feof(incfile))
 	{
 		fputc (c, ofile);
-		c = fgetc (cfile);
+		c = fgetc (incfile);
 	}
-	c = fgetc (tfile);
-	while (!feof(tfile))
+	c = fgetc (tofile);
+	while (!feof(tofile))
 	{
 		fputc (c, ofile);
-		c = fgetc (tfile);
+		c = fgetc (tofile);
 	}
 	exit (0);
 }
